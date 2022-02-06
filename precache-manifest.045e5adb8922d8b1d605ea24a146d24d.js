2c14e192abb85c9ef2ac586106729baa8f0b444f
Tahzeem <tzmkhan007@gmail.com>
2022-02-06 22:04:43 +0530

    Updates

---
+++ precache-manifest.045e5adb8922d8b1d605ea24a146d24d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4035eac42baff69c9787d5ca9cb26a96",
+    "url": "/formValidation_UsingReact/index.html"
+  },
+  {
+    "revision": "6c73ba45ece78af2ce40",
+    "url": "/formValidation_UsingReact/static/css/main.43285f85.chunk.css"
+  },
+  {
+    "revision": "3eba169727bb3b240333",
+    "url": "/formValidation_UsingReact/static/js/2.3500e2a3.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/formValidation_UsingReact/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "6c73ba45ece78af2ce40",
+    "url": "/formValidation_UsingReact/static/js/main.b19cc742.chunk.js"
+  },
+  {
+    "revision": "ed6522ea8f2378402fc5",
+    "url": "/formValidation_UsingReact/static/js/runtime~main.0ea4808b.js"
+  }
+]);
